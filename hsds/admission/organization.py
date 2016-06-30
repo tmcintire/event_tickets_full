eaa54b16244f15d842644a7077d93d04d1c233b8
timmcintire <tm1618>
2016-06-30 13:58:11 -0500

    fix organiztion name py file

--- hsds/admission/organization.py
+++ hsds/admission/organization.py
@@ -5,4 +5,6 @@ def organization(request):
     # This needs to be organized in some way to allow multiple organizations to have multiple events
     organization_name = Organization.objects.get(pk=1)
 
+    if organization_name is None:
+        organization_name = "Event Organizer"
     return locals()
