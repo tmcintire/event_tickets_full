e2ed251a2415fa4b5653fd103a39c260e014a6c6
timmcintire <tm1618>
2016-06-30 14:01:55 -0500

    more fixes

--- hsds/admission/organization.py
+++ hsds/admission/organization.py
@@ -3,6 +3,5 @@ from models import Organization
 
 def organization(request):
     # This needs to be organized in some way to allow multiple organizations to have multiple events
-    #organization_name = Organization.objects.get(pk=1)
 
     return locals()
